e67f9da23e36ba104de1f4082766fa08cf76ae68
Anastasia Kapustina <nikitinaan98@mail.ru>
2024-02-14 18:14:45 +0300

    refactor: code style refactor

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 import '../styles/index.css';
 import { getRandomInt } from './utils';
 
-let startSpeed = 10;
+let startSpeed = 20;
 let count = 0;
 
 const canvas = document.getElementById('game');
@@ -21,9 +21,36 @@ const apple = {
   y: getRandomInt(0, cellsCount) * grid
 };
 
+function checkOverflow() {     
+  if (snake.x < 0) {
+    snake.x = canvas.width - grid;
+  }
+  else if (snake.x >= canvas.width) {
+    snake.x = 0;
+  }
+  if (snake.y < 0) {
+    snake.y = canvas.height - grid;
+  }
+  else if (snake.y >= canvas.height) {
+    snake.y = 0;
+  }
+};
+
+function renderApple() {
+  apple.x = getRandomInt(0, cellsCount) * grid;
+  apple.y = getRandomInt(0, cellsCount) * grid;
+
+  for (let i = 0; i < snake.cells.length; i++) {
+    if (apple.x === snake.cells[i].x && apple.y === snake.cells[i].y) {
+      renderApple();
+      break;
+    }
+  }
+};
+
 function loop() {
     requestAnimationFrame(loop);
-    //замедляет скорость игры с 60 кадров в секунду до 10. пропускает каждый 10
+    //замедляет скорость игры с 60 кадров в секунду до 3. пропускает каждый 20
     if (++count < startSpeed) {
       return;
     }
@@ -31,63 +58,38 @@ function loop() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     snake.x += snake.dx;
     snake.y += snake.dy;
-    
-    if (snake.x < 0) {
-      snake.x = canvas.width - grid;
-    }
-    else if (snake.x >= canvas.width) {
-      snake.x = 0;
-    }
-    if (snake.y < 0) {
-      snake.y = canvas.height - grid;
-    }
-    else if (snake.y >= canvas.height) {
-      snake.y = 0;
-    }
 
-
-    // Продолжаем двигаться в выбранном направлении. Голова всегда впереди, поэтому добавляем её координаты в начало массива, который отвечает за всю змейку.
+    checkOverflow(); 
     snake.cells.unshift({ x: snake.x, y: snake.y });
-    // Сразу после этого удаляем последний элемент из массива змейки, потому что она движется и постоянно особождает клетки после себя
     if (snake.cells.length > snake.maxCells) {
       snake.cells.pop();
     }
 
     context.fillStyle = '#d12527';
     context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
-    // Одно движение змейки — один новый нарисованный квадратик 
     context.fillStyle = '#bdda57';
-    // Обрабатываем каждый элемент змейки
-    snake.cells.forEach(function (cell, index) {
-      // Чтобы создать эффект клеточек, делаем зелёные квадратики меньше на один пиксель, чтобы вокруг них образовалась чёрная граница
+
+    snake.cells.forEach(function (cell) {
       context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
-      // Если змейка добралась до яблока...
-      if (cell.x === apple.x && cell.y === apple.y) {
-        // увеличиваем длину змейки
-        snake.maxCells++;
-        // Рисуем новое яблочко
-        // Помним, что размер холста у нас 400x400, при этом он разбит на ячейки — 25 в каждую сторону
-        apple.x = getRandomInt(0, cellsCount) * grid;
-        apple.y = getRandomInt(0, cellsCount) * grid;
-      }
-      // Проверяем, не столкнулась ли змея сама с собой
-      // Для этого перебираем весь массив и смотрим, есть ли у нас в массиве змейки две клетки с одинаковыми координатами 
-      for (let i = index + 1; i < snake.cells.length; i++) {
-        // Если такие клетки есть — начинаем игру заново
-        if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-          // Задаём стартовые параметры основным переменным
-          snake.x = getRandomInt(0, cellsCount) * grid;
-          snake.y = getRandomInt(0, cellsCount) * grid;
-          snake.cells = [];
-          snake.maxCells = 3;
-          snake.dx = grid;
-          snake.dy = 0;
-          // Ставим яблочко в случайное место
-          apple.x = getRandomInt(0, cellsCount) * grid;
-          apple.y = getRandomInt(0, cellsCount) * grid;
-        }
-      }
     });
+
+    for (let i = 1; i < snake.cells.length; i++) {
+      if (snake.x === snake.cells[i].x && snake.y === snake.cells[i].y) {
+        snake.x = getRandomInt(0, cellsCount) * grid;
+        snake.y = getRandomInt(0, cellsCount) * grid;
+        snake.cells = [];
+        snake.maxCells = 3;
+        snake.dx = grid;
+        snake.dy = 0;
+
+        renderApple();
+      }
+    }
+
+    if (snake.x === apple.x && snake.y === apple.y) {
+      snake.maxCells++;
+      renderApple();
+    }
   }
 
 document.addEventListener('keydown', function (e) {
